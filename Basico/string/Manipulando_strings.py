d3f8259d44bac7c0ae4f3c6f1c0a0bd672edd549
Júlia Rodrigues (Jua) <105869092+juliarmn@users.noreply.github.com>
2023-02-04 15:15:02 -0300

    Update Manipulando_strings.py

--- Basico/string/Manipulando_strings.py
+++ Basico/string/Manipulando_strings.py
@@ -41,6 +41,9 @@ custo = 2.5
 print('O valor do produto é {} e o custo de produção é {}'.format(valor, custo))
 #Outra forma:
 print('{0}  {1}  {0} '.format(valor, custo))#Imprime valor custo valor
+#Outra forma:
+texto = 'O valor do produto é {}'
+print(texto.format(valor))
 
 #Udo do %:
 print('O valor do produto é: %f; \nO custo de produção é: %f' % (valor, custo))
@@ -51,4 +54,80 @@ print('AAAA' in 'aaAAAAbbBBBb')
 #retorna true ou false - no caso acima é true
 
 #Métodos das strings:
+#capitalize(): Coloca a primeira em maiúscula
+nome = 'júlia'
+print(nome.capitalize())
 
+#casefold(): Coloca todo o texto em minúsculo
+nome_2 = 'JULIA'
+print(nome_2.casefold())#pode usar o lower tbm - melhor casefold
+
+#count(): quantas vezes aparece na string essa substring
+simbolos = '$$566665555%%%%##'
+print(simbolos.count('%'))
+
+#endswith(): Verifica se o texto acaba com aquela substring
+email_2 = 'Marta.Silva@gmail.com'
+print(email_2.endswith('.com'))
+#Retorna true ou false
+#É para verificações
+
+#find():
+#Procura um texto dentro de outro texto e dá como resposta a posição em que aparece
+print(email_2.find('@'))
+
+#isalnum():
+#Verifica se a string tem numero e letras
+texto_2 = 'Jua123'
+print(texto_2.isalnum())#retorna true ou false
+
+#isalpha():
+#Verifica se o texto é todo composto de letras:
+texto_3 = 'asdasd'
+print(texto_3.isalpha())#retorna true ou false
+
+#isnumeric():
+#Verifica se o texto é todo composto de letras:
+texto_4 = '12323'
+print(texto_4.isnumeric())#retorna true ou false
+
+#replace():
+#Troca um texto por outro
+numero = '1090.80'
+print(numero.replace('.', ','))#dois argumentos
+
+#split():
+#Separa a string de acordo com algum delimitador
+codigo = 'BABABA%123'
+print(codigo.split('%'))
+#resultado:
+#['BABABA', '123']
+
+#Textos de mais de uma linha:
+texto_grande = '''
+                 Olá, bom dia
+
+                 Como está?
+                 '''
+
+#splitlines():
+#Divide textos de mais de uma linha no enter
+print(texto_grande.splitlines())
+
+#stratswith():
+#Verifica se começa com um texto -> como endswith
+
+#strip():
+#Retira caracteres "indesejados"
+nome_user = ' Julia.rm '
+print(nome_user.strip())#retira os espaços extra - retira antes e no final
+
+#title():
+#Coloca cada caracter inicial da palavra em maiúsculo
+nome_e_sobrenome = 'júlia rodrigues marques do nascimento'
+print(nome_e_sobrenome.title())
+
+#upper():
+#Texto todo em letra maiúscula:
+cod = 'asdasdasdasd'
+print(cod.upper())
