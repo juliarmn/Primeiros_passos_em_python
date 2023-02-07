d65b01a5b196852f7fa9eb1124f5d62129843d8e
Júlia Rodrigues (Jua) <105869092+juliarmn@users.noreply.github.com>
2023-02-07 18:32:21 -0300

    Create Basico.py

---
+++ Basico/Estruturas_de_repeticao/Basico.py
@@ -0,0 +1,32 @@
+#for: repetição do comando
+produtos = ['coca', 'guaraná', 'suco', 'cerveja']
+producao = [100, 2000, 550, 3200]
+tamanho = len(produtos)
+for i in range(tamanho):
+  print('Produto: {};\nProdução: {}\n\n'.format(produtos[i], producao[i]))
+  print('---------------------------------------------')
+#i é o índice e é incrementado a cada iteração
+
+#for each: forma de manipular cada item da lista, sem o uso do índice:
+for produto in produtos:
+  print('O produto é {}'.format(produto))#Printa cada item
+
+#Enumerate:
+#Pega o item e variável
+funcionarios = ['Júlia', 'Gabriel', 'Paula', 'Diego']
+for func,i in enumerate(funcionarios):
+  print('Nome: {};\nÍndice: {}\n\n'.format(func, i))
+  print('---------------------------------------------')
+  
+#Continue e break:
+#break: interrompe e sai do laço
+fruta = ['maçã', 'abacate', 'abacaxi', 'melancia'] 
+for item in fruta:
+  if item == 'abacaxi':
+    break
+tam = len(fruta)
+for i in range(tam):
+  if fruta[i] == 'abacate':
+    continue
+  else: 
+    print(fruta[i])
