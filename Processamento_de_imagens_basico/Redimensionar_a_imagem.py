4577f24781c2023f369af5299ae9635a527fccf4
Júlia Rodrigues (Jua) <105869092+juliarmn@users.noreply.github.com>
2023-02-10 18:24:01 -0300

    Update Redimensionar_a_imagem.py

--- Processamento_de_imagens_basico/Redimensionar_a_imagem.py
+++ Processamento_de_imagens_basico/Redimensionar_a_imagem.py
@@ -17,6 +17,23 @@ imagem = cv2.resize(imagem, (largura_nova, altura_nova))
 cv2.imshow("Imagem Original", imagem)
 cv2.waitKey(0)
 #Para não perder a proporção:
+imagem = cv2.imread("imagem.jpg")
 largura = imagem.shape[1]
 altura = imagem.shape[0]
 prop = float(altura/largura)#Definir a proporção da imagem
+largura_nova = 90
+altura_nova = int(largura_nova * prop)
+imagem = cv2.resize(imagem, (largura_nova, altura_nova))
+cv2.imshow("Imagem", imagem)
+cv2.waitKey(0)
+
+#Rotacionando imagens:
+imagem = cv2.imread("imagem.jpg")
+rotacionada = imagem[:, ::-1]
+#Usando ::-1 colocamos a lista de trás para frente->inverte os pixels das imagens
+cv2.imshow("Imagem Rotacionada", rotacionada)#rotacionou no eixo x
+cv2.waitKey(0)
+#Rotacionar em y:
+rotacionada = imagem[::-1, :]
+#Rotacionar em ambos os eixos:
+rotacionada = imagem[::-1, ::-1]
