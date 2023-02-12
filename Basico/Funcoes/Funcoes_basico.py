cd1252cc20673d22aaaa50b8268e6548d2c210bc
Júlia Rodrigues (Jua) <105869092+juliarmn@users.noreply.github.com>
2023-02-12 20:11:27 -0300

    Update Funcoes_basico.py

--- Basico/Funcoes/Funcoes_basico.py
+++ Basico/Funcoes/Funcoes_basico.py
@@ -24,3 +24,12 @@ def padroniza_cod(codigos, padrao = 'M'):#padroniza tudo para letra maiuscula
 return codigos
      
 #Lembre, com return, a function encerra imediatamente
+
+#Return com mais de um objeto:
+#Coloca os valores em uma tupla e devolde ela como resposta
+def multiplicacao_divisao (num_1, num_2):
+  multiplicacao = num_1 * num_2
+  divisao = num_1 * num_2
+  return(multiplicacao, divisao)#Retornou uma tupla -> não precisa dos (), pois por padrão ele retorna uma tupla
+
+
