73f28ddfb8afc489892ced373abcfdb6898a0265
Júlia Rodrigues (Jua) <105869092+juliarmn@users.noreply.github.com>
2023-03-04 12:51:57 -0300

    Create set.py

---
+++ Basico/Estruturas_de_repeticao/set.py
@@ -0,0 +1,8 @@
+set = {'A', 'B', 'F', 'G', 'N', 'M'}
+#Não tem uma ordem - ordem não é fixa
+#Não apresenta elementos repetidos
+#É um iterable
+cpf_clientes = ['486.753.373-45', '161.253.071-07', '366.537.715-39', '228.643.413-10', '215.534.908-43', '361.353.557-24', '361.353.557-24', '161.253.071-07']
+print(len(cpf_clientes))#Apenas o tamanho
+set_cpf = set(cpf_clientes)
+print(len(set_cpf))#Quantidade real de clientes
