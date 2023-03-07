8637babf7c701e4bba485d90e1f3622a515e18c4
Júlia Rodrigues (Jua) <105869092+juliarmn@users.noreply.github.com>
2023-03-07 10:12:03 -0300

    Create intro.py

---
+++ Google Earth Engine/intro.py
@@ -0,0 +1,25 @@
+import ee
+ee.Initialize()
+coordenadas =  "-3.10719,  -60.0261, -3.9000, -64.5000"
+x1, y1, x2, y2 = coordenadas.split(",")
+geometria = geometry = ee.Geometry.Polygon (
+                                            [
+                                                [[float(x1), float(y2)],
+                                                 [float(x1), float(y1)],
+                                                 [float(x2), float(y2)],
+                                                 [float(x2), float(y1)]
+                                                ]
+                                            ]
+                                            )
+#Queremos as coordenadas centrais da área:
+longitude_centro = (float(y2) + float(y1)) / 2
+latitude_centro = (float(x2) + float(x1)) / 2
+#Definir por data as imagens que queremos:
+data = "2022-06-01, 2022-06-06"
+#Unpacking:
+inicio, fim = data.split(",")
+#Filtrar coleções:
+#1 - Pegar a coleção desejada, vamos usar o landsat-08
+colecao = ee.ImageCollection("LANDSAT/LC08/C02/T1").filterBounds(geometria).filterDate(inicio, fim).filterMetadata('CLOUD_COVER', 'less_than', 100)
+#Ver o total de imagens disponíveis:
+#print(colecao.size().getInfo()) -> verificar depois porque está dando problema   
