3d40ba673eb59a2cc289c3911fa14b2ba4d84f83
Júlia Rodrigues (Jua) <105869092+juliarmn@users.noreply.github.com>
2023-03-07 11:00:50 -0300

    Update intro.py

--- Google Earth Engine/intro.py
+++ Google Earth Engine/intro.py
@@ -15,11 +15,13 @@ geometria = geometry = ee.Geometry.Polygon (
 longitude_centro = (float(y2) + float(y1)) / 2
 latitude_centro = (float(x2) + float(x1)) / 2
 #Definir por data as imagens que queremos:
-data = "2022-06-01, 2022-06-06"
+data = "2017-01-01,2017-12-31"
 #Unpacking:
 inicio, fim = data.split(",")
+print(inicio)
+print(fim)
 #Filtrar coleções:
 #1 - Pegar a coleção desejada, vamos usar o landsat-08
 colecao = ee.ImageCollection("LANDSAT/LC08/C02/T1").filterBounds(geometria).filterDate(inicio, fim).filterMetadata('CLOUD_COVER', 'less_than', 100)
 #Ver o total de imagens disponíveis:
-#print(colecao.size().getInfo()) -> verificar depois porque está dando problema   
+print(colecao.size().getInfo())
