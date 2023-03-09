06652f6beaa472a7983e4ad7fe38b3cead3798d7
Júlia Rodrigues (Jua) <105869092+juliarmn@users.noreply.github.com>
2023-03-09 15:44:42 -0300

    Create Exercicio_1_simples.py

---
+++ Basico/Lista_comprehension/Exercicio_1_simples.py
@@ -0,0 +1,6 @@
+#A lista abaixo apresenta os produtos, a qauntidade vendida em 2019 e, por fim, a vendida em 2020
+#Faça por list compreension apenas os produtos de 2019 -> ideia de exercício da hashtag treinamentos
+vendas = [('A', 456, 890), ('B', 780, 880), ('C', 900, 809)]
+
+lista_2019 = [prod_2019 for prod, prod_2019, prod_2020 in vendas]
+print(lista_2019)
