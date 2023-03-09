addcb6f2af47f64c1a7b49544fc9efadfb260001
Júlia Rodrigues (Jua) <105869092+juliarmn@users.noreply.github.com>
2023-03-09 17:07:36 -0300

    Update Exercicio_4.py

--- Basico/Lista_comprehension/Exercicio_4.py
+++ Basico/Lista_comprehension/Exercicio_4.py
@@ -1,5 +1,17 @@
 #% das vendas totais dos top 3 produtos:
-form collections import Counter
-produtos = [('A', 300), ('B', 789), ('C', 726), ('D', 908), ('E', 33), ('F', 456), ('G', 890)]
-top_3 = Counter(produtos).most_common(3)
+i = 0
+top_3 = []
+total = 0
+produtos = [(300, 'A'), (789, 'B'), (756, 'C'), (908, 'D'), (33, 'E'), (456, 'F'), (890, 'G')]
+for num, prod in produtos:
+    total += num
+print(total)
+produtos.sort(reverse=True)
+while i < 3:
+    top_3.append(produtos[i])
+    i += 1
 print(top_3)
+total_top_3 = 0
+total_top_3 = sum(num for num, produto in top_3)
+print(total_top_3)
+print('% Top 3 do total: {:0.2%}'.format(total_top_3 / total))
