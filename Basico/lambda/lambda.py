192eeae649df5c8b311affab9aea5bdf93260c7f
Júlia Rodrigues (Jua) <105869092+juliarmn@users.noreply.github.com>
2023-03-09 17:42:33 -0300

    Create lambda.py

---
+++ Basico/lambda/lambda.py
@@ -0,0 +1,29 @@
+#Funções em iterables:
+#Método para aplicar a função em cada item do iterable
+#função map
+#list_2 = list(map(função, iterable incial))
+def padronizar_cpf(cpf):
+    cpf = '{}.{}.{}-{}'.format(cpf[:3], cpf[3:6], cpf[6:9], cpf[9:])
+    return cpf
+  
+cpf = ['02874334472', '33518043420', '55621733100', '54261536390', '97564615273']
+for i, item in enumerate(cpf):
+  cpf[i] = padronizar_cpf(item)
+print(cpf)
+#Usando o map
+cpf = list(map(padronizar_cpf, cpf))
+print(cpf)
+
+#Usando function no iterable no sort:
+#sort aplica uma function em cada elemento da lista
+#sort modifica a lista inicial
+#sorted cria uma nova lista
+#no sort temos o key (chave para ordenar) como parâmetro
+#Podemos usar a key para ordenar tudo como se fosse letra minúscula sem modificar a lista, por exemplo
+celulares = ['apple', 'Samsung', 'Iphone', 'galaxy']
+#Com o sort apenas, sem a key (usa ASCII):
+celulares.sort()
+print(celulares)
+#Com a key:
+celulares.sort(key = str.casefold)
+print(celulares)
