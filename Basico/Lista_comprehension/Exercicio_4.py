185894c47ad8852e3949a347ec4178337f15cf95
Júlia Rodrigues (Jua) <105869092+juliarmn@users.noreply.github.com>
2023-03-09 16:55:50 -0300

    Update Exercicio_4.py

--- Basico/Lista_comprehension/Exercicio_4.py
+++ Basico/Lista_comprehension/Exercicio_4.py
@@ -1,1 +1,5 @@
 #% das vendas totais dos top 3 produtos:
+form collections import Counter
+produtos = [('A', 300), ('B', 789), ('C', 726), ('D', 908), ('E', 33), ('F', 456), ('G', 890)]
+top_3 = Counter(produtos).most_common(3)
+print(top_3)
