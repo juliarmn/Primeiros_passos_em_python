295d6f16cf084e100dfacd6cacb8e2b7ac13cee6
Júlia Rodrigues (Jua) <105869092+juliarmn@users.noreply.github.com>
2023-03-09 15:47:14 -0300

    Update Exercicio_1_simples.py

--- Basico/Lista_comprehension/Exercicio_1_simples.py
+++ Basico/Lista_comprehension/Exercicio_1_simples.py
@@ -4,3 +4,6 @@ vendas = [('A', 456, 890), ('B', 780, 880), ('C', 900, 809)]
 
 lista_2019 = [prod_2019 for prod, prod_2019, prod_2020 in vendas]
 print(lista_2019)
+#Descubra o valor mais vendido em 2019:
+lista_2019_2 = [(prod, prod_2019) for  prod, prod_2019, prod_2020 in vendas]
+print(max(lista_2019_2))
